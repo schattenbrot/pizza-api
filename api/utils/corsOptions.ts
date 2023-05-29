eef7a681eb36848f6eb8a8ce4169211cc786b9e6
Schattenbrot <aaron.machill@web.de>
2023-05-29 17:30:18 +0200

    add cors config

---
+++ api/utils/corsOptions.ts
@@ -0,0 +1,19 @@
+import cors from 'cors';
+import { NODE_ENV, SERVER } from './environment';
+import logger from './logger';
+
+const protocol = NODE_ENV === 'production' ? 'https' : 'http';
+const origin = `${protocol}://${SERVER}`;
+
+const corsOptions = {
+  origin,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Length'],
+  credentials: true,
+  maxAge: 12 * 3600,
+};
+
+logger.info(`The configured server origin is: ${origin}`);
+
+export default corsOptions;
--- api/utils/express.ts
+++ api/utils/express.ts
@@ -1,10 +1,14 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 import routes from '../routes';
+import corsOptions from './corsOptions';
 import errorHandler from './errorHandler';
 import swaggerDocs from './swagger';
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
